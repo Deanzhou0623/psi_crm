c93a36e8aa319e362205fa659e3f5c86a8d390a9
deanZhou <1182160314@qq.com>
2024-01-14 21:40:49 -0800

    Initial commit

---
+++ src/main/java/com/roadjava/psi_crm/PsiCrmApplication.java
@@ -0,0 +1,13 @@
+package com.roadjava.psi_crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsiCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsiCrmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/roadjava/psi_crm/PsiCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roadjava.psi_crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsiCrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
